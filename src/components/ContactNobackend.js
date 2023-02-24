13de62e0e91c6335fd8caff0c9b61c236f8caf39
Lightinthebulb <>
2023-02-24 10:30:44 -0500

    Updated About me & Included Message on Contact Page.

--- src/components/ContactNobackend.js
+++ src/components/ContactNobackend.js
@@ -1,48 +1,70 @@
-import React, { useState } from 'react'
-import emailjs from 'emailjs-com'
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
-import './ContactNobackend.scss'
+import "./ContactNobackend.scss";
 
 const Contact = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [emailSent, setEmailSent] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
 
-    const submit = () => {
-        if (name && email && message) {
-            const serviceId = 'service_id';
-            const templateId = 'template_id';
-            const userId = 'user_id';
-            const templateParams = {
-                name,
-                email,
-                message
-            };
+  const submit = () => {
+    if (name && email && message) {
+      const serviceId = "service_id";
+      const templateId = "template_id";
+      const userId = "user_id";
+      const templateParams = {
+        name,
+        email,
+        message,
+      };
 
-            emailjs.send(serviceId, templateId, templateParams, userId)
-                .then(response => console.log(response))
-                .then(error => console.log(error));
+      emailjs
+        .send(serviceId, templateId, templateParams, userId)
+        .then((response) => console.log(response))
+        .then((error) => console.log(error));
 
-            setName('');
-            setEmail('');
-            setMessage('');
-            setEmailSent(true);
-        } else {
-            alert('Please fill in all fields.');
-        }
+      setName("");
+      setEmail("");
+      setMessage("");
+      setEmailSent(true);
+    } else {
+      alert("Please fill in all fields.");
     }
+  };
 
-    return (
-        <div id="contact-form">
-            <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
-            <input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} />
-            <textarea placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
-            <button onClick={submit}>Send Message</button>
+  return (
+    <div className="contact">
+      <div id="contact-form">
+        <p className="contact-header">Contact</p>
+        <input
+          type="text"
+          placeholder="Your Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Your email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <textarea
+          placeholder="Your message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button onClick={submit}>Send Message</button>
 
-            <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
-        </div>
-    );
+        <span className={emailSent ? "visible" : null}>
+          The functionality of the contact form is under construction. If you
+          would like to give in contact with me, please reach out to me on Linkedin or Twitter.
+        </span>
+        {/* Thank you for your message, I will be in touch very soon! */}
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/pages/About.js
+++ src/pages/About.js
@@ -25,32 +25,30 @@ export default function About() {
             </h2>
             <hr />
             <p className="about-card-text">
-              Hello! My name is Shawn, and I'm a aspiring developer with a
-              passion for building software that solves real-world problems. I'm
-              constantly looking for new ways to improve my skills so that I am
-              able to make a positive impact.
+              Hello! My name is Shawn, and I'm a aspiring software developer
+              with a passion for building software that solves real-world
+              problems. I'm constantly looking for new ways to improve my skills
+              so that I am able to make a positive impact.
             </p>
             <p className="about-card-text">
               I have 5 years of experience working with various computer systems
               and software as an systems admin. I'm always eager to learn new
-              things. I believe in the importance of staying up-to-date with the
+              things. I understand the importance of staying up-to-date with the
               latest developments in the tech industry, and I make a point to
               keep my skills sharp by attending conferences, reading tech blogs,
               and collaborating with other.
             </p>
             <p className="about-card-text">
               I'm a big believer in work-life balance, and I strive to maintain
-              a healthy balance between my professional and personal pursuits.
-              <br></br>
-              If you're interested in working together or learning more about my
-              work, please don't hesitate to reach out me on twitter. If you
-              would like to checkout any of my previous projects or my current
-              projects, "check out my GitHub", etc.)]. I look forward to hearing
-              from you!
+              a healthy balance between my professional and personal
+              pursuits. Outside of coding, I enjoy fishing, sports, and all things
+              movies, television, and gaming!
             </p>
             <p className="about-card-text">
-              Outside of coding, I enjoy music, sports, and all things movies,
-              television, and gaming!
+              If you're interested in working together or learning more about my
+              work, please don't hesitate to reach out me on twitter. If you
+              would like to see what I have been working on, check out my
+              Github. I look forward to hearing from you!
             </p>
           </div>
         </div>
